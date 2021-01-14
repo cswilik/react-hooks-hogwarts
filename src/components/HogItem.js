b67f5f6b3b37f2da59e86b378867cc1470a50ad9
ARElton <ameliareneeelton@gmail.com>
2021-01-14 13:45:58 -0500

    post lunch push

--- src/components/HogItem.js
+++ src/components/HogItem.js
@@ -5,18 +5,29 @@ import HogImages from "./HogImages"
 
 function HogItem({ name, specialty, greased, weight, metal }) {
 
-    function handleClick() {
-        const otherInfo = <p>{specialty}</p>
-        return(
-        otherInfo
-        
-        )}
+    // function handleClick() {
+    //     // console.log("hello")
+    //     return( 
+    //     <fragment>
+    //         <h3>{ specialty }</h3>
+    //         <h3>{ greased }</h3>
+    //         <h3>{ weight }</h3>
+    //         <h3>{ metal }</h3>
+    //     </fragment>
+    //     )
+    // }
 
     return (
-        <div id={name} className="ui eight wide column" onClick={handleClick}>
+        <div id={name} className="ui eight wide column" >
             <h3>{name}</h3>
-            {otherInfo}
-            <HogImages name= {name}/>
+            
+            {/* <p>{returnHog}</p> */}
+            <HogImages name= {name}
+                specialty = { specialty }
+                greased = { greased }
+                weight = { weight }
+                metal = { metal }
+            />
         </div>
     )
 }
